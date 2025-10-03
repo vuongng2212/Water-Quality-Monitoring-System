9fc668439b84ff3e13477eee37352171ac78c517
Vuong Nguyen <kudoyaung@gmail.com>
2025-10-03 21:34:08 +0700

    feat: add authentication and user services

---
+++ backend/src/main/java/iuh/backend/service/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package iuh.backend.service;
+
+import iuh.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        iuh.backend.model.User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
+    }
+}
