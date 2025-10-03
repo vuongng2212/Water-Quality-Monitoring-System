5c4e2c7c270e59a3b6f90b3946ae17eabd2f11df
Vuong Nguyen <kudoyaung@gmail.com>
2025-10-03 21:35:35 +0700

    feat: add request and response payload classes

---
+++ backend/src/main/java/iuh/backend/payload/response/LoginResponse.java
@@ -0,0 +1,14 @@
+package iuh.backend.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponse {
+    private String token;
+}
