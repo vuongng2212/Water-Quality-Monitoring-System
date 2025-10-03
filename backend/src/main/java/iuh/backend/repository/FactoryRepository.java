09f5a403bf5882c9316f410291c9ddb436394f99
Vuong Nguyen <kudoyaung@gmail.com>
2025-10-03 21:33:55 +0700

    feat: add repositories for Factory and User entities

---
+++ backend/src/main/java/iuh/backend/repository/FactoryRepository.java
@@ -0,0 +1,9 @@
+package iuh.backend.repository;
+
+import iuh.backend.model.Factory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FactoryRepository extends JpaRepository<Factory, Long> {
+}
