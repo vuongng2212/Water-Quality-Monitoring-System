8e38b88158bc6671f4e26122be66509f433f13ac
Vuong Nguyen <kudoyaung@gmail.com>
2025-11-08 18:52:26 +0700

    feat(security): cho phép truy cập endpoint thiết bị trong SecurityConfig

--- backend/src/main/java/iuh/backend/config/SecurityConfig.java
+++ backend/src/main/java/iuh/backend/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource())) // Thêm cấu hình CORS
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req ->
-                        req.requestMatchers("/api/auth/**", "/api/sensor-data/**",
+                        req.requestMatchers("/api/auth/**", "/api/sensor-data/**", "/api/device/**",
                                 "/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html")
                                 .permitAll()
                                 .anyRequest()
